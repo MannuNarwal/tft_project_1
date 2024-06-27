c5759579e0a63bd94b83a79bb19bb3c2819ff506
Manisha Narwal <narwalmanish1@gmail.com>
2024-06-27 10:53:44 +0530

    changed_code_as_pom

---
+++ helper_func.py
@@ -0,0 +1,60 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from xfile import *
+
+
+def getUrlDriver(url):
+    options = webdriver.ChromeOptions()
+    # options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--ignore-ssl-errors')
+
+    # options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+    return driver
+  
+
+# Clicking archives and show button
+def click_button(driver, by, xpath):
+    try:
+        button = driver.find_element(by, xpath)
+        button.click()
+        sleep(1)
+    except Exception as e:
+        print(f"An error occurred while clicking one click button: {e}")
+
+# Selecting gold, all and session2 button
+def dropdown_button(driver,by, xpath1, xpath2):
+    try:
+        button = driver.find_element(by, xpath1)
+        button.click()
+        sleep(1)
+        button = driver.find_element(by, xpath2)
+        button.click()
+        sleep(1)
+    except Exception as e:
+        print(f"An error occurred while clicking dropdown button: {e}")
+
+# Selecting date button
+def select_date(driver,by,x1, x2, x3, x4):
+    try:
+        button = driver.find_element(by, x1)
+        button.click()
+        sleep(1)
+        button = driver.find_element(by, x2)
+        button.click()
+        sleep(1)
+        button = driver.find_element(by,x3)
+        button.click()
+        sleep(1)
+        button = driver.find_element(by,x4)
+        button.click()
+        sleep(1)
+    except Exception as e:
+        print(f"An error occurred while clicking date button: {e}")
+        
+def getElementData(driver, by, xpath):
+    el = driver.find_element(by, xpath)
+    return el.text
---
+++ scenerio.py
@@ -0,0 +1,50 @@
+import pytest
+from helper_func import *
+from selenium.webdriver.common.by import By
+from xfile import *
+
+
+def test_open_website():
+  driver = getUrlDriver(main_url)
+  opened_url = driver.current_url
+  assert opened_url == main_url, "FAILED - URL opened is NOT correct URL.!!"
+
+def test_no_filter_selected():
+  driver = getUrlDriver(main_url)
+  click_button(driver, By.CLASS_NAME, archive_button)
+  click_button(driver, By.XPATH,show_button)
+  data = getElementData(driver, By.CLASS_NAME, err_class)
+  assert data == NO_SELECTED, "FAILED - Wrong error message"
+  
+def test_only_commodity():
+  driver = getUrlDriver(main_url)
+  click_button(driver, By.CLASS_NAME, archive_button)
+  dropdown_button(driver, By.XPATH, gold_dropdown_xpath1,select_gold_xpath2)
+  click_button(driver, By.XPATH,show_button)
+  data = getElementData(driver, By.CLASS_NAME, err_class)
+  assert data == ONLY_COMMODITY_SELECTED, "FAILED - Wrong error message"
+
+def test_from_date():
+  driver = getUrlDriver(main_url)
+  click_button(driver, By.CLASS_NAME, archive_button)
+  dropdown_button(driver, By.XPATH, gold_dropdown_xpath1,select_gold_xpath2)
+  select_date(driver,By.XPATH,FROM_x1,FROM_year_x2,FROM_month_x3,FROM_date_x4)
+  click_button(driver, By.XPATH,show_button)
+  data = getElementData(driver, By.CLASS_NAME, err_class)
+  assert data == ONLY_DATE_SELECTED, "FAILED - Wrong error message"
+  
+
+  
+def test_different_date():
+  driver = getUrlDriver(main_url)
+  click_button(driver, By.CLASS_NAME, archive_button)
+  dropdown_button(driver, By.XPATH, gold_dropdown_xpath1,select_gold_xpath2)
+  select_date(driver,By.XPATH,FROM_x1,TO_year_x2,TO_month_x3,TO_date_x4)
+  select_date(driver,By.XPATH,TO_x1,FROM_year_x2,FROM_month_x3,FROM_date_x4)
+  click_button(driver, By.XPATH,show_button)
+  data = getElementData(driver, By.CLASS_NAME, err_class)
+  assert data == DIFFERENT_DATE, "FAILED - Wrong error message"
+
+
+
+
